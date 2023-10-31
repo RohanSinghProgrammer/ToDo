fd928370e1bfa3505685b5a6115ee4e1b88b5283
Rohan Singh <ff3870422@gmail.com>
2023-10-31 20:59:58 +0530

    ui completed

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -10,29 +10,43 @@ type Params = {
 const Todos = ({ tab }: Params) => {
   const { todos, markAsCompleted, deleteTodo } = useTodos();
 
-    let filterTodos = todos;
-    if (tab === "active") {
-      filterTodos = todos.filter((item) => !item.isCompleted);
-    } else if (tab === "completed") {
-      filterTodos = todos.filter((item) => item.isCompleted);
-    }
+  let filterTodos = todos;
+  if (tab === "active") {
+    filterTodos = todos.filter((item) => !item.isCompleted);
+  } else if (tab === "completed") {
+    filterTodos = todos.filter((item) => item.isCompleted);
+  }
+
+  const done = "line-through text-red-600 flex-1 px-4";
+  const notDone = "text-gray-900 flex-1 px-4";
 
   return (
     <div className="w-full">
       {filterTodos.map((item) => (
-        <div key={item.id} className="w-full flex items-center p-2 h-14 border-b hover:bg-red-100">
+        <div
+          key={item.id}
+          className="w-full flex items-center p-2 h-14 border-b hover:bg-red-50"
+        >
           <input
             type="checkbox"
             id={`todo-${item.id}`}
             checked={item.isCompleted}
             onChange={() => markAsCompleted(item.id)}
-            className="bg-red-500"
+            className="accent-red-500"
           />
-          <label htmlFor={`todo-${item.id}`} className="flex-1 px-4">
+          <label
+            htmlFor={`todo-${item.id}`}
+            className={item.isCompleted ? done : notDone}
+          >
             {item.task}
           </label>
           {item.isCompleted && (
-            <button onClick={() => deleteTodo(item.id)} className="px-4 py-2 bg-red-500 text-white rounded-md">DELETE</button>
+            <button
+              onClick={() => deleteTodo(item.id)}
+              className="px-4 py-2 bg-red-500 text-white rounded-md"
+            >
+              DELETE
+            </button>
           )}
         </div>
       ))}
