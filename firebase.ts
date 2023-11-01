41fc19886015b15a86d1117894a6637ea79ff9f3
Rohan Singh <ff3870422@gmail.com>
2023-11-01 11:49:21 +0530

    adding firebase

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVEk4OfPd4smy_vII3zliNSbpXpSUYm3o",
+  authDomain: "next-todo-41726.firebaseapp.com",
+  projectId: "next-todo-41726",
+  storageBucket: "next-todo-41726.appspot.com",
+  messagingSenderId: "539609204613",
+  appId: "1:539609204613:web:2af98a75e414653f24e6ec",
+  measurementId: "G-9691PCZFYD",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const provider = new GoogleAuthProvider();
+export const auth = getAuth();
